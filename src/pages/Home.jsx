c95033bd2f3d9ab836bcb68bfc26a758d185b31d
Mehroj <mehrojhamroev261@gmail.com>
2025-07-07 17:15:41 +0500

    menu changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="xl:flex flex-col justify-center items-center bg-cover bg-center h-screen md:w-full flex flex-col sm:flex flex-col justify-center items-center  w-full pt-[20px]" id='home' style={{ backgroundImage: `url(${img})` }}>
       <h1 className='text-white text-xl pl-[100px]  md:text-4xl xl:text-6xl mx-auto sm:text-4xl  '>FRESH COFFEE IN THE MORNING</h1>
       <p className=' sm: w-full flex items-center justify-center text-center text-white xl:text-white text-xl xl: pt-[50px] mx-auto'>Lorem Ipsum Dolor, Sit Amet Consectetur Adipisicing Elit. Quidem Voluptatem, Natus Illo Nulla At Inventore.</p>
-      <button className=' hover:w-[300px] tracking-widest transition-all duration-500 ease-in-out 
+      <button className='  hover:w-[300px] tracking-widest transition-all duration-500 ease-in-out 
                     w-[200px] h-[50px] text-white bg-amber-500 mt-[30px]  mx-auto '>Get Yours Now</button>
     </div>
   )
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -13,49 +13,79 @@ const Menu = () => {
       <h1 className='w-full h-[75px] text-5xl text-white bg-black font-bold flex items-center justify-center pt-5'>OUR <span className='text-amber-300'>MENU</span></h1>
       <div className='grid grid-cols-1 px-4 md:px-64 gap-7 sm:grid-cols-2 lg:grid-cols-3'>
         <div className='flex flex-col items-center justify-center gap-5 bg-black text-white border border-white hover:bg-white hover:text-black py-6'>
-          <img src={menu1} alt="menu1" />
+          <img src={menu4} alt="menu4" />
           <h3 className='text-2xl font-normal'>Tasty And Healthy</h3>
-          <p>$15.99 <span className='line-through'>20.99</span></p>
-          <button className='sm:hover:w-2/3 xl:hover:w-[300px] tracking-widest transition-all duration-500 ease-in-out 
-                    px-4 py-3 text-white bg-amber-500 mt-[30px]  mx-auto '>Get Yours Now</button>
+          <p>$15.99 <span className='line-through'>$20.99</span></p>
+
+          <button
+            className='w-1/2 hover:w-2/3 tracking-widest transition-all duration-500 ease-in-out 
+               px-4 py-3 text-white bg-amber-500 mt-[30px] mx-auto'>
+            Get Yours Now
+          </button>
         </div>
+
         <div className='flex flex-col items-center justify-center gap-5 bg-black text-white border border-white hover:bg-white hover:text-black py-6'>
-          <img src={menu2} alt="menu2" />
+          <img src={menu4} alt="menu4" />
           <h3 className='text-2xl font-normal'>Tasty And Healthy</h3>
-          <p>$15.99 <span className='line-through'>20.99</span></p>
-          <button className='sm:hover:w-2/3 xl:hover:w-[300px] tracking-widest transition-all duration-500 ease-in-out 
-                    px-4 py-3 text-white bg-amber-500 mt-[30px]  mx-auto '>Get Yours Now</button>
+          <p>$15.99 <span className='line-through'>$20.99</span></p>
+
+          <button
+            className='w-1/2 hover:w-2/3 tracking-widest transition-all duration-500 ease-in-out 
+               px-4 py-3 text-white bg-amber-500 mt-[30px] mx-auto'>
+            Get Yours Now
+          </button>
         </div>
+
         <div className='flex flex-col items-center justify-center gap-5 bg-black text-white border border-white hover:bg-white hover:text-black py-6'>
-          <img src={menu3} alt="menu3" />
+          <img src={menu4} alt="menu4" />
           <h3 className='text-2xl font-normal'>Tasty And Healthy</h3>
-          <p>$15.99 <span className='line-through'>20.99</span></p>
-          <button className='sm:hover:w-2/3 xl:hover:w-[300px] tracking-widest transition-all duration-500 ease-in-out 
-                    px-4 py-3 text-white bg-amber-500 mt-[30px]  mx-auto '>Get Yours Now</button>
+          <p>$15.99 <span className='line-through'>$20.99</span></p>
+
+          <button
+            className='w-1/2 hover:w-2/3 tracking-widest transition-all duration-500 ease-in-out 
+               px-4 py-3 text-white bg-amber-500 mt-[30px] mx-auto'>
+            Get Yours Now
+          </button>
         </div>
+
 
 
         <div className='flex flex-col items-center justify-center gap-5 bg-black text-white border border-white hover:bg-white hover:text-black py-6'>
           <img src={menu4} alt="menu4" />
           <h3 className='text-2xl font-normal'>Tasty And Healthy</h3>
-          <p>$15.99 <span className='line-through'>20.99</span></p>
-          <button className='sm:hover:w-2/3 xl:hover:w-[300px] tracking-widest transition-all duration-500 ease-in-out 
-                    px-4 py-3 text-white bg-amber-500 mt-[30px]  mx-auto '>Get Yours Now</button>
+          <p>$15.99 <span className='line-through'>$20.99</span></p>
+
+          <button
+            className='w-1/2 hover:w-2/3 tracking-widest transition-all duration-500 ease-in-out 
+               px-4 py-3 text-white bg-amber-500 mt-[30px] mx-auto'>
+            Get Yours Now
+          </button>
         </div>
+
         <div className='flex flex-col items-center justify-center gap-5 bg-black text-white border border-white hover:bg-white hover:text-black py-6'>
-          <img src={menu5} alt="menu5" />
+          <img src={menu4} alt="menu4" />
           <h3 className='text-2xl font-normal'>Tasty And Healthy</h3>
-          <p>$15.99 <span className='line-through'>20.99</span></p>
-          <button className='sm:hover:w-2/3 xl:hover:w-[300px] tracking-widest transition-all duration-500 ease-in-out 
-                    px-4 py-3 text-white bg-amber-500 mt-[30px]  mx-auto '>Get Yours Now</button>
+          <p>$15.99 <span className='line-through'>$20.99</span></p>
+
+          <button
+            className='w-1/2 hover:w-2/3 tracking-widest transition-all duration-500 ease-in-out 
+               px-4 py-3 text-white bg-amber-500 mt-[30px] mx-auto'>
+            Get Yours Now
+          </button>
         </div>
-        <div className='flex flex-col items-center justify-center gap-5 bg-black text-white border border-white hover:bg-white hover:text-black py-2'>
-          <img src={menu6} alt="menu6" />
-          <h3 className='text-2xl font-normal text-center'>Tasty And Healthy</h3>
-          <p>$15.99 <span className='line-through'>20.99</span></p>
-          <button className='sm:hover:w-[150px] xl:hover:w-[300px] transition-all duration-500 ease-in-out 
-                   px-4 py-3 text-white bg-amber-500 mt-[30px]  mx-auto '>Get Yours Now</button>
+
+        <div className='flex flex-col items-center justify-center gap-5 bg-black text-white border border-white hover:bg-white hover:text-black py-6'>
+          <img src={menu4} alt="menu4" />
+          <h3 className='text-2xl font-normal'>Tasty And Healthy</h3>
+          <p>$15.99 <span className='line-through'>$20.99</span></p>
+
+          <button
+            className='w-1/2 hover:w-2/3 tracking-widest transition-all duration-500 ease-in-out 
+               px-4 py-3 text-white bg-amber-500 mt-[30px] mx-auto'>
+            Get Yours Now
+          </button>
         </div>
+
       </div>
     </div>
   )
