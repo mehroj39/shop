cdd65df9379f21378519aeb8de99085b5c4a37a8
Mehroj <mehrojhamroev261@gmail.com>
2025-07-08 10:49:24 +0500

    blogs page changed

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -11,7 +11,7 @@ const Blogs = () => {
           CONTACT <span className='text-amber-300'>US</span>
         </h1>
         <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 p-7 gap-7 pb-[30-px] '>
-          <div className='flex flex-col gap-7 bg-black border border-white '>
+          <div className='flex flex-col gap-7 bg-black border border-white w-full'>
             <img src={pic1} alt="pic1" />
             <h2 className='px-7 text-3xl font-bold text-amber-300'>Tasty And Refreshing Coffee</h2>
             <h3 className='px-7 text-white text-2xl font-bold'>By Admin / 7th july ,2025</h3>
@@ -20,7 +20,7 @@ const Blogs = () => {
              whitespace-nowrap overflow-hidden 
              tracking-widest px-4 py-3 text-white bg-amber-500 mt-[30px]  mb-[30px] mx-auto'>Read More</button>
           </div>
-          <div className='flex flex-col gap-7 bg-black border border-white '>
+          <div className='flex flex-col gap-7 bg-black border border-white w-full '>
             <img src={pic2} alt="pic1" />
             <h2 className='px-7 text-3xl font-bold text-amber-300'>Tasty And Refreshing Coffee</h2>
             <h3 className='px-7 text-white text-2xl font-bold'>By Admin / 7th july ,2025</h3>
@@ -29,7 +29,7 @@ const Blogs = () => {
              whitespace-nowrap overflow-hidden 
              tracking-widest px-4 py-3 text-white bg-amber-500 mt-[30px] mb-[30px] mx-auto'>Read More</button>
           </div>
-          <div className='flex flex-col gap-7 bg-black border border-white '>
+          <div className='flex flex-col gap-7 bg-black border border-white w-full '>
             <img src={pic3} alt="pic1" />
             <h2 className='px-7 text-3xl font-bold text-amber-300'>Tasty And Refreshing Coffee</h2>
             <h3 className='px-7 text-white text-2xl font-bold'>By Admin / 7th july ,2025</h3>
